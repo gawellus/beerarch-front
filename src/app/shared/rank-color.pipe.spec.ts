0d83f123ad6d91c009e7f7ad9f7127a1c1568474
gawellus <pmorawiak@gmail.com>
2020-11-16 15:43:04 +0100

    rank color pipe

---
+++ src/app/shared/rank-color.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RankColorPipe } from './rank-color.pipe';
+
+describe('RankColorPipe', () => {
+  it('create an instance', () => {
+    const pipe = new RankColorPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/shared/rank-color.pipe.ts
@@ -0,0 +1,30 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'rankcolor'
+})
+export class RankColorPipe implements PipeTransform {
+
+private color: string;
+
+  transform(rank): unknown {
+    let y: number = +rank;    
+    if (y <= 2) {
+      this.color = "#E40613";
+    }
+    else if (y <= 4) {
+      this.color = "#FE9307";
+    }
+    else if (y <= 6) {
+      this.color = "#FFD200";
+    }
+    else if (y <= 8) {
+      this.color = "#BCEB00";
+    }
+    else {
+      this.color = "#27A738";
+    }
+    return this.color;
+  }
+
+}
